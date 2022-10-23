677da7c6b1c2f0ac50ec1336d206664a3cd991f2
Adolfo <adlf.aldana@gmail.com>
2022-10-23 10:23:44 -0400

    #feat: show image titles

--- gif-expert-app/src/components/GifGrid.jsx
+++ gif-expert-app/src/components/GifGrid.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-  getGifs(category);
+  const [imagenes, setImagenes] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImagenes(newImages);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
   return (
     <>
       <h3>{category}</h3>
+      <ol>
+        {imagenes.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ol>
     </>
   );
 };
