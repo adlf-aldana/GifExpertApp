62256545f2e400c307f7af1050385ef4aabee182
Adolfo <adlf.aldana@gmail.com>
2022-10-23 00:15:11 -0400

    #feat: add new category

--- gif-expert-app/src/GifExpertApp.jsx
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -3,12 +3,16 @@ import { useState } from "react";
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
+  const onAddCategory = () => {
+    setCategories([...categories, "Valorant"]);
+  };
+
   return (
     <>
       {/* TITULO */}
       <h1>GifExpertApp</h1>
       {/* INPUT */}
-
+      <button onClick={onAddCategory}>Agregar</button>
       {/* LISTADO DE GIF */}
       <ol>
         {categories.map((category) => (
